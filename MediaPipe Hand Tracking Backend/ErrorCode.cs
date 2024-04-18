3706a4551126c7da6733303d4e9f6f27eb404a9b
kubakCz <jakubis.s@seznam.cz>
2024-04-18 23:21:09 +0200

    Add recording start and stop functionality

---
+++ MediaPipe Hand Tracking Backend/ErrorCode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPipe_Hand_Tracking_Backend
+{
+    public enum ErrorCode
+    {
+        OK,
+        Internal,
+        External,
+        Network,
+        Other,
+        InvalidArgumnet,
+        InvalidOperation
+    }
+
+    public static class ErrorCodeExtensions
+    {
+        public static string Description(this ErrorCode error) => error switch
+        {
+            ErrorCode.OK => "Operation successful",
+            ErrorCode.Internal => "Suspect internal errors. Contact support.",
+            ErrorCode.External => "External errors. Make sure correct parameters are used for input arguments when calling the methods.",
+            ErrorCode.Network => "The error occurred on the network side.",
+            ErrorCode.Other => "Unlisted error is conflicting the method call.",
+            ErrorCode.InvalidArgumnet => "Invalid input arguments have been inputted.",
+            ErrorCode.InvalidOperation => "Invalid operation.",
+            _ => $"Unknown error code {error}."
+        };
+    }
+}
--- MediaPipe Hand Tracking Backend/Program.cs
+++ MediaPipe Hand Tracking Backend/Program.cs
@@ -1,43 +1,98 @@
 ﻿using NatNetML;
 
-class Program
+namespace MediaPipe_Hand_Tracking_Backend
 {
-    /*  [NatNet] Network connection configuration    */
-    private static NatNetClientML natNet = new NatNetClientML();    // The client instance
-    private static string strLocalIP = "127.0.0.1";                 // Local IP address (string)
-    private static string strServerIP = "127.0.0.1";                // Server IP address (string)
-    private static ushort serverCommandPort = 1510;
-    private static ushort serverDataPort = 1511;
-    private static ConnectionType connectionType = ConnectionType.Multicast;    // Connection Type
-
-    public static async Task Main(string[] args)
+    class Program
     {
-        await ConnectToServer();
-    }
+        /*  [NatNet] Network connection configuration    */
+        private static NatNetClientML natNet = new NatNetClientML();    // The client instance
+        private static string strLocalIP = "127.0.0.1";                 // Local IP address (string)
+        private static string strServerIP = "127.0.0.1";                // Server IP address (string)
+        private static ushort serverCommandPort = 1510;
+        private static ushort serverDataPort = 1511;
+        private static ConnectionType connectionType = ConnectionType.Multicast;    // Connection Type
 
-    static async Task ConnectToServer()
-    {
-        /*  [NatNet] Checking verions of the NatNet SDK library  */
-        int[] verNatNet = new int[4];
-        verNatNet = natNet.NatNetVersion();
-        Console.WriteLine("NatNet SDK Version: {0}.{1}.{2}.{3}", verNatNet[0], verNatNet[1], verNatNet[2], verNatNet[3]);
+        private static bool isRecording = false;
 
-        /*  [NatNet] Connecting to the Server    */
-        Console.WriteLine($"\nConnecting...\n\tLocal IP address: {strLocalIP}\n\tServer IP Address: {strServerIP}\n\tCommand port: {serverCommandPort}\n\tData port: {serverDataPort}");
-        var connect_options = new NatNetClientML.ConnectParams()
+        public static async Task Main(string[] args)
         {
-            ConnectionType = connectionType,
-            ServerAddress = strServerIP,
-            LocalAddress = strLocalIP,
-            ServerCommandPort = serverCommandPort,
-            ServerDataPort = serverDataPort
-        };
-        
-        while(natNet.Connect(connect_options) != 0)
-        {
-            Console.WriteLine("Unable to connect to the NatNet server. Trying again in 10 seconds.");
-            await Task.Delay(10 * 1000);
+            await ConnectToServer();
+            while (true)
+            {
+                if (Console.ReadKey(true).Key == ConsoleKey.S)
+                {
+                    if (isRecording)
+                        Console.WriteLine(StopRecording());
+                    else
+                        Console.WriteLine(StartRecording());
+                }
+            }
         }
-        Console.WriteLine("Successfully connected to the server.");
+
+        static async Task ConnectToServer()
+        {
+            /*  [NatNet] Checking verions of the NatNet SDK library  */
+            int[] verNatNet = new int[4];
+            verNatNet = natNet.NatNetVersion();
+            Console.WriteLine("NatNet SDK Version: {0}.{1}.{2}.{3}", verNatNet[0], verNatNet[1], verNatNet[2], verNatNet[3]);
+
+            /*  [NatNet] Connecting to the Server    */
+            Console.WriteLine($"\nConnecting...\n\tLocal IP address: {strLocalIP}\n\tServer IP Address: {strServerIP}\n\tCommand port: {serverCommandPort}\n\tData port: {serverDataPort}");
+            var connect_options = new NatNetClientML.ConnectParams()
+            {
+                ConnectionType = connectionType,
+                ServerAddress = strServerIP,
+                LocalAddress = strLocalIP,
+                ServerCommandPort = serverCommandPort,
+                ServerDataPort = serverDataPort
+            };
+
+            while (natNet.Connect(connect_options) != 0)
+            {
+                Console.WriteLine("Unable to connect to the NatNet server. Trying again in 10 seconds.");
+                await Task.Delay(10 * 1000);
+            }
+            Console.WriteLine("Successfully connected to the server.");
+        }
+
+        static ErrorCode StartRecording()
+        {
+            if (isRecording)
+                throw new InvalidOperationException("Recording already in progress.");
+
+            int nBytes = 0;
+            byte[] response = new byte[1000];
+            ErrorCode ec = (ErrorCode)natNet.SendMessageAndWait("StartRecording", 3, 100, out response, out nBytes);
+
+            if (ec is not ErrorCode.OK)
+                return ec;
+            
+            int opResult = BitConverter.ToInt32(response, 0);
+            if (opResult != 0)
+                throw new Exception("No idea what that means, but the command failed.");
+            
+            isRecording = true;
+            return ErrorCode.OK;
+        }
+
+        static ErrorCode StopRecording()
+        {
+            if (!isRecording)
+                throw new InvalidOperationException("Recording not in progress.");
+
+            int nBytes = 0;
+            byte[] response = new byte[1000];
+            ErrorCode ec = (ErrorCode)natNet.SendMessageAndWait("StopRecording", 3, 100, out response, out nBytes);
+
+            if (ec is not ErrorCode.OK)
+                return ec;
+
+            int opResult = BitConverter.ToInt32(response, 0);
+            if (opResult != 0)
+                throw new Exception("No idea what that means, but the command failed.");
+
+            isRecording = false;
+            return ErrorCode.OK;
+        }
     }
 }
